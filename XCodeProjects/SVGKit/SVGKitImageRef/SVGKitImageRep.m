5701ab4e2f8b59ed10d8af8c0f51db91baec065b
C.W. Betts <computers57@hotmail.com>
2012-12-05 17:57:19 -0700

    The SVGKitImageRef should be working. There's isn't a way to test it yet.

--- XCodeProjects/SVGKit/SVGKitImageRef/SVGKitImageRep.m
+++ XCodeProjects/SVGKit/SVGKitImageRef/SVGKitImageRep.m
@@ -30,10 +30,72 @@
 	return UTItypes;
 }
 
++ (NSArray *)imageUnfilteredPasteboardTypes
+{
+	/* TODO */
+	return nil;
+}
+
++ (BOOL)canInitWithData:(NSData *)d
+{
+	SVGDocument *tempDoc = [[SVGDocument alloc] initWithData:d];
+	if (tempDoc == nil) {
+		return NO;
+	}
+	[tempDoc release];
+	return YES;
+}
+
++ (NSImageRep *)imageRepWithData:(NSData *)d
+{
+	return [[[self alloc] initWithData:d] autorelease];
+}
 
 + (void)load
 {
 	[NSImageRep registerImageRepClass:[SVGKitImageRep class]];
+}
+
+- (id)initWithData:(NSData *)theData
+{
+	if (self = [super init]) {
+		_document = [[SVGDocument alloc] initWithData:theData];
+		[self setColorSpaceName:NSCalibratedRGBColorSpace];
+		[self setAlpha:YES];
+		[self setBitsPerSample:0];
+		[self setOpaque:NO];
+		{
+			
+			NSSize renderSize = NSMakeSize(_document.width, _document.height);
+			[self setSize:renderSize];
+#if CGFLOAT_IS_DOUBLE
+			[self setPixelsHigh:ceil(renderSize.height)];
+			[self setPixelsWide:ceil(renderSize.width)];
+#else
+			[self setPixelsHigh:ceilf(renderSize.height)];
+			[self setPixelsWide:ceilf(renderSize.width)];
+#endif
+
+		}
+
+	}
+	return self;
+}
+
+- (void)dealloc
+{
+	[_document release];
+	
+	[super dealloc];
+}
+
+- (BOOL)draw
+{
+	CGContextRef CGCtx = (CGContextRef)[[NSGraphicsContext currentContext] graphicsPort];
+
+	[self drawLayer:[_document layerTree] inContext:CGCtx];
+	
+	return NO;
 }
 
 @end
