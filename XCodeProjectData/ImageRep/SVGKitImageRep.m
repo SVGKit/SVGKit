97aacbfb56f72c7e4ca73f7e633d68ba21e83b73
C.W. Betts <computers57@hotmail.com>
2013-05-19 16:22:32 -0600

    Use scaleToFitInside: instead of the scale method.

--- XCodeProjectData/ImageRep/SVGKitImageRep.m
+++ XCodeProjectData/ImageRep/SVGKitImageRep.m
@@ -113,6 +113,7 @@
 - (BOOL)draw
 {
 	@autoreleasepool {
+#if 0
 		CGContextRef CGCtx = (CGContextRef)[[NSGraphicsContext currentContext] graphicsPort];
 		
 		CGAffineTransform scaleTrans = CGContextGetCTM(CGCtx);
@@ -120,7 +121,9 @@
 		//Just in case the image is in a differently-sized NSImage
 		//should also work for retina displays
 		_image.scale = MIN(scaleTrans.a, scaleTrans.d);
-		
+#endif
+		[_image scaleToFitInside:self.size];
+
 		NSImage *tmpImage = _image.NSImage;
 		
 		NSRect imageRect;
