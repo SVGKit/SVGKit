216deb84d9ca4e7a3acecaf883453c13fa936646
C.W. Betts <computers57@hotmail.com>
2013-05-19 18:52:17 -0600

    Use float instead of CGFloat: I didn't see the sscanf functions.

--- Source/Unsorted/SVGUtils.m
+++ Source/Unsorted/SVGUtils.m
@@ -399,7 +399,7 @@ CGMutablePathRef createPathFromPointsInString (const char *string, boolean_t clo
 		if (c == '\n' || c == '\t' || c == ' ' || c == ',' || c == '\0') {
 			accum[accumIdx] = '\0';
 			
-			static CGFloat x, y;
+			static float x, y;
 			
 			if (currComponent == 0 && accumIdx != 0) {
 				sscanf( accum, "%g", &x );
