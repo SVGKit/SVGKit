b9ed0c6b8d8f9ec07bd0190bd878af158b39c1b0
C.W. Betts <computers57@hotmail.com>
2013-05-25 15:37:29 -0600

    Implement some pattern code for OS X. Expand the iOS pattern code to accept CGImages and CGColors.

--- Source/Unsorted/SVGKPattern.m
+++ Source/Unsorted/SVGKPattern.m
@@ -19,6 +19,66 @@
     return [self patternWithUIColor:patternImage];
 }
 
++ (SVGKPattern*)patternWithCGImage:(CGImageRef)cgImage
+{
+	return [self patternWithImage:[UIImage imageWithCGImage:cgImage]];
+}
+
++ (SVGKPattern*)patternWithCGColor:(CGColorRef)cgColor
+{
+	return [self patternWithUIColor:[UIColor colorWithCGColor:cgColor]];
+}
+
+#else
+
+//Code taken from TBColor from https://github.com/zrxq/TBColor
+static void ImagePatternCallback (void *imagePtr, CGContextRef ctx) {
+    CGContextDrawImage(ctx, CGRectMake(0, 0, CGImageGetWidth(imagePtr), CGImageGetHeight(imagePtr)), imagePtr);
+}
+
+static void ImageReleaseCallback(void *imagePtr) {
+    CGImageRelease(imagePtr);
+}
+
+static CGColorRef CGColorMakeFromImage(CGImageRef CF_CONSUMED image) {
+    static const CGPatternCallbacks callback = {0, ImagePatternCallback, ImageReleaseCallback};
+    CGPatternRef pattern = CGPatternCreate(image, NSMakeRect(0, 0, CGImageGetWidth(image), CGImageGetHeight(image)), CGAffineTransformIdentity, CGImageGetWidth(image), CGImageGetHeight(image), kCGPatternTilingConstantSpacing, true, &callback);
+    CGColorSpaceRef coloredPatternColorSpace = CGColorSpaceCreatePattern(NULL);
+    CGFloat dummy = 1.0f;
+    CGColorRef color = CGColorCreateWithPattern(coloredPatternColorSpace, pattern, &dummy);
+    CGColorSpaceRelease(coloredPatternColorSpace);
+    CGPatternRelease(pattern);
+    return color;
+}
+//end taken code
+
++ (SVGKPattern*)patternWithImage:(NSImage*)image
+{
+	CGImageRef quartzImage = [image CGImageForProposedRect:NULL context:NULL hints:NULL];
+	return [self patternWithCGImage:quartzImage];
+}
+
++ (SVGKPattern*)patternWithCGImage:(CGImageRef)cgImage
+{
+	SVGKPattern *p = nil;
+	
+	CGImageRetain(cgImage);
+	CGColorRef tmpColor = CGColorMakeFromImage(cgImage);
+	p = [SVGKPattern patternWithCGColor:tmpColor];
+	CGColorRelease(tmpColor);
+	
+	return p;
+}
+
++ (SVGKPattern*)patternWithCGColor:(CGColorRef)cgColor
+{
+	SVGKPattern *p = [[SVGKPattern alloc] init];
+	
+	p.color = cgColor;
+	
+	return [p autorelease];
+}
+
 #endif
 
 - (CGColorRef)CGColor
@@ -26,7 +86,7 @@
 #if TARGET_OS_IPHONE
     return [self.color CGColor];
 #else
-    return NULL;
+    return self.color;
 #endif
 }
 
