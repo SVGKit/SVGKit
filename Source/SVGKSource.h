ee9c222bc79363843771b2bbd72dc75fc3a53591
C.W. Betts <computers57@hotmail.com>
2013-05-27 16:01:10 -0600

    Make SVGKSource's sourceFromData: function in the header.

--- Source/SVGKSource.h
+++ Source/SVGKSource.h
@@ -23,6 +23,7 @@
 @class SVGKSource;
 
 @interface SVGKSource : NSObject
++ (SVGKSource*)sourceFromData:(NSData*)data;
 
 @property (nonatomic, retain) NSString* svgLanguageVersion; /*< <svg version=""> */
 @property (nonatomic, retain) NSInputStream* stream;
