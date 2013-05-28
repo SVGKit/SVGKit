7e3f6d23e40422b15304c6582a9458e4b20224aa
C.W. Betts <computers57@hotmail.com>
2013-05-28 17:59:34 -0600

    I was being overly paranoid. Reverting my NSString "copying" methods.

--- Source/Sources/SVGKSourceLocalFile.m
+++ Source/Sources/SVGKSourceLocalFile.m
@@ -2,19 +2,6 @@
 
 @implementation SVGKSourceLocalFile
 
-@synthesize filePath = _filePath;
-- (void)setFilePath:(NSString *)filePath
-{
-	if (_filePath != filePath) {
-		[_filePath release];
-		if (filePath) {
-			_filePath = [[NSString alloc] initWithString:filePath];
-		} else {
-			_filePath = nil;
-		}
-	}
-}
-
 + (SVGKSource*)sourceFromFilename:(NSString*)p {
 	NSInputStream* stream = [NSInputStream inputStreamWithFileAtPath:p];
 	[stream open];
