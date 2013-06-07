6b2eae70ffa617a12abaeb195d922c96af211301
C.W. Betts <computers57@hotmail.com>
2013-06-07 15:41:55 -0600

    Initial import of the OS X app.

---
+++ XCodeProjectData/Demo-OSX/SKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SKAppDelegate.h
+//  Demo-OSX
+//
+//  Created by C.W. Betts on 6/7/13.
+//  Copyright (c) 2013 C.W. Betts. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SKAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ XCodeProjectData/Demo-OSX/SKAppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  SKAppDelegate.m
+//  Demo-OSX
+//
+//  Created by C.W. Betts on 6/7/13.
+//  Copyright (c) 2013 C.W. Betts. All rights reserved.
+//
+
+#import "SKAppDelegate.h"
+
+@implementation SKAppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
