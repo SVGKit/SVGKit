79b9b7630b98a23838ea6bbd26c1368a168852cc
C.W. Betts <computers57@hotmail.com>
2013-06-08 15:15:14 -0600

    Set the size to 32 if we don't get passed an image.

--- XCodeProjectData/Demo-OSX/SKAppDelegate.m
+++ XCodeProjectData/Demo-OSX/SKAppDelegate.m
@@ -34,6 +34,8 @@
 		self.layeredView.frameSize = self.fastView.frameSize = anImage.size;
 	} else {
 		self.layeredView.image = self.fastView.image = nil;
+		
+		self.layeredView.frameSize = self.fastView.frameSize = NSMakeSize(32, 32);
 	}
 }
 
