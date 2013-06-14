9030196dc7f1f2ee3cbf05547d59213da79ae42f
C.W. Betts <computers57@hotmail.com>
2013-06-13 19:25:07 -0600

    This should resolve some problems, right?

--- Source/Parsers/SVGKParser.m
+++ Source/Parsers/SVGKParser.m
@@ -545,9 +545,12 @@ static void	charactersFoundSAX (void *ctx, const xmlChar *chars, int len) {
 
 static void errorEncounteredSAX (void *ctx, const char *msg, ...) {
 	va_list va;
+	char errcStr[2048];
 	va_start(va, msg);
-	NSString *errStr = [[NSString alloc] initWithFormat:[NSString stringWithUTF8String:msg] arguments:va];
+	vsprintf(errcStr, msg, va);
 	va_end(va);
+	
+	NSString *errStr = [[NSString alloc] initWithUTF8String:errcStr];
 	DDLogCWarn(@"Error encountered during parse: %@", errStr);
 	SVGKParser *self = parserThatWasMostRecentlyStarted;
 	SVGKParseResult* parseResult = self.currentParseRun;
