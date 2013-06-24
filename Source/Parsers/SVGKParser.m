1742cb8ea2906ccf30094581eb2b26a895bfdced
C.W. Betts <computers57@hotmail.com>
2013-06-23 21:00:54 -0600

    Revert the change of making NSStringFromLibxmlString a macro.

--- Source/Parsers/SVGKParser.m
+++ Source/Parsers/SVGKParser.m
@@ -47,11 +47,7 @@ static void	endElementSAX(void *ctx, const xmlChar *localname, const xmlChar *pr
 static void	charactersFoundSAX(void * ctx, const xmlChar * ch, int len);
 static void errorEncounteredSAX(void * ctx, const char * msg, ...);
 
-#if 0
 static NSString *NSStringFromLibxmlString (const xmlChar *string);
-#else
-#define NSStringFromLibxmlString(string) @((const char *) string)
-#endif
 static NSMutableDictionary *NSDictionaryFromLibxmlNamespaces (const xmlChar **namespaces, int namespaces_ct);
 static NSMutableDictionary *NSDictionaryFromLibxmlAttributes (const xmlChar **attrs, int attr_ct);
 
@@ -641,14 +637,12 @@ static xmlSAXHandler SAXHandler = {
 #pragma mark -
 #pragma mark Utility
 
-#if 0
 static NSString *NSStringFromLibxmlString (const xmlChar *string) {
 	if( string == NULL ) // Yes, Apple requires we do this check!
 		return nil;
 	else
 		return [NSString stringWithUTF8String:(const char *) string];
 }
-#endif
 
 static NSMutableDictionary *NSDictionaryFromLibxmlNamespaces (const xmlChar **namespaces, int namespaces_ct)
 {
