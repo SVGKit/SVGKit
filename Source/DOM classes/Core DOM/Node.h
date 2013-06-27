4fc5c5c9e1a5be1e5a3bd00434de8d63c0950b3f
C.W. Betts <computers57@hotmail.com>
2013-06-27 16:43:55 -0600

    Change a weak reference to unsafe_unretained that's present in the iOS code.

--- Source/DOM classes/Core DOM/Node.h
+++ Source/DOM classes/Core DOM/Node.h
@@ -147,7 +147,7 @@ typedef enum DOMNodeType
 // DOM Level 3 that we *need*, partly because SVG Spec makes one brief reference to it: http://www.w3.org/TR/SVG/text.html#InterfaceSVGTextContentElement
 
 // Introduced in DOM Level 3: http://www.w3.org/TR/2004/REC-DOM-Level-3-Core-20040407/core.html#Node3-textContent
-@property(weak, nonatomic,readonly) NSString* textContent;
+@property(unsafe_unretained, nonatomic,readonly) NSString* textContent;
 
 #pragma mark - Objective-C init methods (not in SVG Spec - you're supposed to use SVGDocument's createXXX methods instead)
 /** Designated initializers - 2 pairs (one for DOM 1, no namespace, the other for DOM 2, with namespace) of 2 methods (one for nodes that REQUIRE a value, the other for nodes that MUST NOT have a value) */
