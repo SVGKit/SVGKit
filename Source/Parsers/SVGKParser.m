50ba343cd47c553239756a9e8ab4c52ee2a59ad5
C.W. Betts <computers57@hotmail.com>
2013-06-26 23:42:34 -0600

    Minor spacing change.

--- Source/Parsers/SVGKParser.m
+++ Source/Parsers/SVGKParser.m
@@ -535,11 +535,13 @@ static void	charactersFoundSAX (void *ctx, const xmlChar *chars, int len) {
 }
 
 static void errorEncounteredSAX (void *ctx, const char *msg, ...) {
-	va_list va;
 	char errcStr[2048] = {0};
-	va_start(va, msg);
-	vsprintf(errcStr, msg, va);
-	va_end(va);
+	{
+		va_list va;
+		va_start(va, msg);
+		vsprintf(errcStr, msg, va);
+		va_end(va);
+	}
 	
 	NSString *errStr = [[NSString alloc] initWithUTF8String:errcStr];
 	SVGKParser *NSctx = ctx;
