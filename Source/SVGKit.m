a865272f55c9dcf6a42c38d93a8c8b408e0eac6b
C.W. Betts <computers57@hotmail.com>
2013-07-01 00:15:50 -0600

    Adding more verbosity to SVGKit's setRawLevel for developers.

--- Source/SVGKit.m
+++ Source/SVGKit.m
@@ -75,6 +75,16 @@ static dispatch_once_t rawLogLevelToken;
 
 + (void) setRawLogLevel:(int)rawLevel withWarning:(BOOL)warn
 {
+#define LOGFLAGCHECK(theFlag, mutStr, logVal) \
+if ((logVal & theFlag) == theFlag) { \
+NSString *tmpStr = @(#theFlag); \
+if (mutStr.length == 0) { \
+[mutStr setString:tmpStr]; \
+} else { \
+[mutStr appendFormat:@" %@", tmpStr]; \
+} \
+}
+	
 	if (warn) {
 		dispatch_once(&rawLogLevelToken, ^{
 			NSLog(RAWLEVELWARNSTR, self);
@@ -86,33 +96,34 @@ static dispatch_once_t rawLogLevelToken;
 		newLogLevel &= ((int)LOG_LEVEL_VERBOSE);
 		NSMutableString *valString = [[NSMutableString alloc] init];
 		@autoreleasepool {
-#define LOGFLAGCHECK(theFlag) \
-if ((newLogLevel & theFlag) == theFlag) { \
-NSString *tmpStr = @(#theFlag); \
-if (valString.length == 0) { \
-[valString setString:tmpStr]; \
-} else { \
-[valString appendFormat:@" %@", tmpStr]; \
-} \
-}
-			
-			LOGFLAGCHECK(LOG_FLAG_VERBOSE);
-			LOGFLAGCHECK(LOG_FLAG_INFO);
-			LOGFLAGCHECK(LOG_FLAG_WARN);
-			LOGFLAGCHECK(LOG_FLAG_ERROR);
-			
-#undef LOGFLAGCHECK
+			LOGFLAGCHECK(LOG_FLAG_VERBOSE, valString, newLogLevel);
+			LOGFLAGCHECK(LOG_FLAG_INFO, valString, newLogLevel);
+			LOGFLAGCHECK(LOG_FLAG_WARN, valString, newLogLevel);
+			LOGFLAGCHECK(LOG_FLAG_ERROR, valString, newLogLevel);
+			if (valString.length == 0) {
+				[valString setString:@"LOG_LEVEL_OFF"];
+			}
 		}
-		
-		if (valString.length == 0) {
-			[valString setString:@"LOG_LEVEL_OFF"];
-		}
-		NSLog(@"[%@] WARN: The raw log level %i is invalid! The new raw log level is %i, or %@.", self, rawLevel, newLogLevel, valString);
+		LOG_OBJC_MAYBE(LOG_ASYNC_INFO, (ddLogLevelInternal | newLogLevel), LOG_FLAG_INFO, 0, @"[%@] WARN: The raw log level %i is invalid! The new raw log level is %i, or %@.", self, rawLevel, newLogLevel, valString);
 		[valString release];
 		ddLogLevelInternal = newLogLevel;
 	}else {
+		@autoreleasepool {
+			NSMutableString *valStr = [NSMutableString string];
+			
+			LOGFLAGCHECK(LOG_FLAG_VERBOSE, valStr, rawLevel);
+			LOGFLAGCHECK(LOG_FLAG_INFO, valStr, rawLevel);
+			LOGFLAGCHECK(LOG_FLAG_WARN, valStr, rawLevel);
+			LOGFLAGCHECK(LOG_FLAG_ERROR, valStr, rawLevel);
+			if (valStr.length == 0) {
+				[valStr setString:@"LOG_LEVEL_OFF"];
+			}
+			
+			LOG_OBJC_MAYBE(LOG_ASYNC_VERBOSE, (ddLogLevelInternal | rawLevel), LOG_FLAG_VERBOSE, 0, @"[%@] DEBUG: Current raw debug level has been set at %i, or %@", self, rawLevel, valStr);
+		}
 		ddLogLevelInternal = rawLevel;
 	}
+#undef LOGFLAGCHECK
 }
 
 + (void) setRawLogLevel:(int)rawLevel
