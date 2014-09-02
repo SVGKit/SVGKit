7d3936e98f28edbd73c62a34af220d3002708ef9
C.W. Betts <computers57@hotmail.com>
2014-09-02 17:22:20 -0600

    Add a Swift iOS demo.

---
+++ Swift-iOS/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  Swift-iOS
+//
+//  Created by C.W. Betts on 9/2/14.
+//  Copyright (c) 2014 na. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+                            
+	@IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+	var detailItem: AnyObject? {
+		didSet {
+		    // Update the view.
+		    self.configureView()
+		}
+	}
+
+	func configureView() {
+		// Update the user interface for the detail item.
+		if let detail: AnyObject = self.detailItem {
+		    if let label = self.detailDescriptionLabel {
+		        label.text = detail.description
+		    }
+		}
+	}
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+		self.configureView()
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
