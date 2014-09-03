28ecfeb63ef77eccc5a228529f5107c16b097c55
C.W. Betts <computers57@hotmail.com>
2014-09-02 21:42:32 -0600

    Most of the Detail view controller has been ported to Swift.

--- Swift-iOS/AppDelegate.swift
+++ Swift-iOS/AppDelegate.swift
@@ -16,13 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
 
 
 	func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        SVGKit.enableLogging()
+        
 		// Override point for customization after application launch.
 		let splitViewController = self.window!.rootViewController as UISplitViewController
 		let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as UINavigationController
 		navigationController.topViewController.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem()
 		splitViewController.delegate = self
-        
-        SVGKit.enableLogging()
         
 		return true
 	}
