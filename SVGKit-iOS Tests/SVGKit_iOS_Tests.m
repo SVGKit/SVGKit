db07381dfbd0235d09bae90b1711eb29cc3b1c31
C.W. Betts <computers57@hotmail.com>
2014-10-13 14:08:35 -0600

    Create a test bundle.

---
+++ SVGKit-iOS Tests/SVGKit_iOS_Tests.m
@@ -0,0 +1,40 @@
+//
+//  SVGKit_iOS_Tests.m
+//  SVGKit-iOS Tests
+//
+//  Created by C.W. Betts on 10/13/14.
+//  Copyright (c) 2014 na. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface SVGKit_iOS_Tests : XCTestCase
+
+@end
+
+@implementation SVGKit_iOS_Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
