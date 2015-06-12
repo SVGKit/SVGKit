8b187e0aa29c2cd867dbd65d3eaaff7a93d6f204
David Gileadi <gileadis@gmail.com>
2015-06-12 07:04:29 -0700

    Improve CSS
    Fixes comment parsing to not span multiple comments, supports comma-separated groups in selectors, supports last-selector-wins, supports most-specific-selector-wins, overrides attributes with CSS styles.

--- Source/DOM classes/Unported or Partial DOM/SVGElement.m
+++ Source/DOM classes/Unported or Partial DOM/SVGElement.m
@@ -426,6 +426,30 @@
 	return self;
 }
 
+- (NSRange) nextSelectorGroupFromText:(NSString *) selectorText startFrom:(NSRange) previous
+{
+    previous.location = previous.location + previous.length;
+    if( previous.location < selectorText.length )
+    {
+        if( [selectorText characterAtIndex:previous.location] == ',' )
+            previous.location = previous.location + 1;
+        
+        NSCharacterSet *whitespace = [NSCharacterSet whitespaceAndNewlineCharacterSet];
+        while( previous.location < selectorText.length && [whitespace characterIsMember:[selectorText characterAtIndex:previous.location]] )
+            previous.location = previous.location + 1;
+        
+        if( previous.location < selectorText.length ) {
+            previous.length = selectorText.length - previous.location;
+            NSRange nextGroup = [selectorText rangeOfString:@"," options:0 range:previous];
+            if( nextGroup.location == NSNotFound )
+                return previous;
+            else
+                return NSMakeRange(previous.location, nextGroup.location - previous.location);
+        }
+    }
+    return NSMakeRange(NSNotFound, -1);
+}
+
 - (NSRange) nextSelectorRangeFromText:(NSString *) selectorText startFrom:(NSRange) previous
 {
     NSCharacterSet *alphaNum = [NSCharacterSet alphanumericCharacterSet];
@@ -438,7 +462,10 @@
         unichar c = [selectorText characterAtIndex:i];
         if( [selectorStart characterIsMember:c] )
         {
-            start = i;
+            if( start == -1 )
+                start = i;
+            else
+                break;
         }
         else if( [alphaNum characterIsMember:c] )
         {
@@ -458,30 +485,62 @@
         return NSMakeRange(NSNotFound, -1);
 }
 
-- (BOOL) selector:(NSString *)selector appliesTo:(SVGElement *) element
+- (BOOL) selector:(NSString *)selector appliesTo:(SVGElement *) element specificity:(NSInteger*) specificity
 {
     if( [selector characterAtIndex:0] == '.' )
-        return element.className != nil && [element.className isEqualToString:[selector substringFromIndex:1]];
+    {
+        if( element.className != nil && [element.className isEqualToString:[selector substringFromIndex:1]] )
+        {
+            *specificity += 100;
+            return YES;
+        }
+    }
     else if( [selector characterAtIndex:0] == '#' )
-        return element.identifier != nil && [element.identifier isEqualToString:[selector substringFromIndex:1]];
-    else
-        return element.nodeName != nil && [element.nodeName isEqualToString:selector];
+    {
+        if( element.identifier != nil && [element.identifier isEqualToString:[selector substringFromIndex:1]] )
+        {
+            *specificity += 10000;
+            return YES;
+        }
+    }
+    else if( element.nodeName != nil && [element.nodeName isEqualToString:selector] )
+    {
+        *specificity += 1;
+        return YES;
+    }
+    else if( [selector isEqualToString:@"*"] )
+    {
+        return YES;
+    }
+    return NO;
 }
 
-- (BOOL) styleRule:(CSSStyleRule *) styleRule appliesTo:(SVGElement *) element
+- (BOOL) styleRule:(CSSStyleRule *) styleRule appliesTo:(SVGElement *) element specificity:(NSInteger*) specificity
 {
-    NSRange nextRule = [self nextSelectorRangeFromText:styleRule.selectorText startFrom:NSMakeRange(0, 0)];
-    if( nextRule.location == NSNotFound )
-        return NO;
-    
-    while( nextRule.location != NSNotFound )
+    NSRange nextGroup = [self nextSelectorGroupFromText:styleRule.selectorText startFrom:NSMakeRange(0, 0)];
+    while( nextGroup.location != NSNotFound )
     {
-        if( ![self selector:[styleRule.selectorText substringWithRange:nextRule] appliesTo:element] )
-            return NO;
+        NSRange nextRule = [self nextSelectorRangeFromText:styleRule.selectorText startFrom:NSMakeRange(nextGroup.location, 0)];
         
-        nextRule = [self nextSelectorRangeFromText:styleRule.selectorText startFrom:nextRule];
+        BOOL match = nextRule.location != NSNotFound;
+        while( nextRule.location != NSNotFound )
+        {
+            if( ![self selector:[styleRule.selectorText substringWithRange:nextRule] appliesTo:element specificity:specificity] )
+            {
+                match = NO;
+                break;
+            }
+            nextRule = [self nextSelectorRangeFromText:styleRule.selectorText startFrom:nextRule];
+            if( nextRule.location > (nextGroup.location + nextGroup.length) )
+                break;
+        }
+        
+        if( match )
+            return YES;
+        
+        nextGroup = [self nextSelectorGroupFromText:styleRule.selectorText startFrom:nextGroup];
     }
-    return YES;
+    return NO;
 }
 
 #pragma mark - CSS cascading special attributes
@@ -505,72 +564,80 @@
 	 
 	 ********* WAWRNING: THE CURRENT IMPLEMENTATION BELOW IS VEYR MUCH INCOMPLETE, BUT IT WORKS FOR VERY SIMPLE SVG'S ************
 	 */
-	if( [self hasAttribute:stylableProperty])
-		return [self getAttribute:stylableProperty];
-	else
-	{
-		NSString* localStyleValue = [self.style getPropertyValue:stylableProperty];
-		
-		if( localStyleValue != nil )
-			return localStyleValue;
-		else
-		{
-            /** we have a locally declared CSS class; let's go hunt for it in the document's stylesheets */
-            
-            @autoreleasepool /** DOM / CSS is insanely verbose, so this is likely to generate a lot of crud objects */
+    NSString* localStyleValue = [self.style getPropertyValue:stylableProperty];
+    
+    if( localStyleValue != nil )
+        return localStyleValue;
+    
+    /** we have a locally declared CSS class; let's go hunt for it in the document's stylesheets */
+    
+    @autoreleasepool /** DOM / CSS is insanely verbose, so this is likely to generate a lot of crud objects */
+    {
+        CSSStyleRule *mostSpecificRule = nil;
+        NSInteger mostSpecificity = -1;
+        
+        for( StyleSheet* genericSheet in self.rootOfCurrentDocumentFragment.styleSheets.internalArray.reverseObjectEnumerator ) // because it's far too much effort to use CSS's low-quality iteration here...
+        {
+            if( [genericSheet isKindOfClass:[CSSStyleSheet class]])
             {
-                for( StyleSheet* genericSheet in self.rootOfCurrentDocumentFragment.styleSheets.internalArray ) // because it's far too much effort to use CSS's low-quality iteration here...
+                CSSStyleSheet* cssSheet = (CSSStyleSheet*) genericSheet;
+                
+                for( CSSRule* genericRule in cssSheet.cssRules.internalArray.reverseObjectEnumerator)
                 {
-                    if( [genericSheet isKindOfClass:[CSSStyleSheet class]])
+                    if( [genericRule isKindOfClass:[CSSStyleRule class]])
                     {
-                        CSSStyleSheet* cssSheet = (CSSStyleSheet*) genericSheet;
+                        CSSStyleRule* styleRule = (CSSStyleRule*) genericRule;
                         
-                        for( CSSRule* genericRule in cssSheet.cssRules.internalArray)
+                        if( [styleRule.style getPropertyCSSValue:stylableProperty] != nil )
                         {
-                            if( [genericRule isKindOfClass:[CSSStyleRule class]])
+                            NSInteger ruleSpecificity = 0;
+                            if( [self styleRule:styleRule appliesTo:self specificity:&ruleSpecificity] )
                             {
-                                CSSStyleRule* styleRule = (CSSStyleRule*) genericRule;
-                                
-                                if( [self styleRule:styleRule appliesTo:self] )
-                                {
-                                    return [styleRule.style getPropertyValue:stylableProperty];
+                                if( ruleSpecificity > mostSpecificity ) {
+                                    mostSpecificity = ruleSpecificity;
+                                    mostSpecificRule = styleRule;
                                 }
                             }
                         }
                     }
                 }
-			}
-			
-			/** either there's no class *OR* it found no match for the class in the stylesheets */
-			
-            if( inherit )
-            {
-                /** Finally: move up the tree until you find a <G> node, and ask it to provide the value
-                 OR: if you find an <SVG> tag before you find a <G> tag, give up
-                 */
-                
-                Node* parentElement = self.parentNode;
-                while( parentElement != nil
-                      && ! [parentElement isKindOfClass:[SVGGElement class]]
-                      && ! [parentElement isKindOfClass:[SVGSVGElement class]])
-                {
-                    parentElement = parentElement.parentNode;
-                }
-                
-                if( parentElement == nil
-                   || [parentElement isKindOfClass:[SVGSVGElement class]] )
-                    return nil; // give up!
-                else
-                {
-                    return [((SVGElement*)parentElement) cascadedValueForStylableProperty:stylableProperty];
-                }
             }
-            else
-            {
-                return nil;
-            }
-		}
-	}
+        }
+        
+        if( mostSpecificRule != nil )
+            return [mostSpecificRule.style getPropertyValue:stylableProperty];
+    }
+    
+    /** if there's a local property, use that */
+    if( [self hasAttribute:stylableProperty])
+        return [self getAttribute:stylableProperty];
+    
+    if( inherit )
+    {
+        /** Finally: move up the tree until you find a <G> node, and ask it to provide the value
+         OR: if you find an <SVG> tag before you find a <G> tag, give up
+         */
+        
+        Node* parentElement = self.parentNode;
+        while( parentElement != nil
+              && ! [parentElement isKindOfClass:[SVGGElement class]]
+              && ! [parentElement isKindOfClass:[SVGSVGElement class]])
+        {
+            parentElement = parentElement.parentNode;
+        }
+        
+        if( parentElement == nil
+           || [parentElement isKindOfClass:[SVGSVGElement class]] )
+            return nil; // give up!
+        else
+        {
+            return [((SVGElement*)parentElement) cascadedValueForStylableProperty:stylableProperty];
+        }
+    }
+    else
+    {
+        return nil;
+    }
 }
 
 @end
