70c2b1e41c2fcf1a0173cd6a8256d9a931117c41
Cihat Gündüz <CihatGuenduez@posteo.de>
2015-10-23 13:17:03 +0200

    added framework targets for iOS and tvOS + configured settings in targets

---
+++ SVGKitFramework-iOS/SVGKitFramework-iOS.h
@@ -0,0 +1,19 @@
+//
+//  SVGKitFramework-iOS.h
+//  SVGKitFramework-iOS
+//
+//  Created by Cihat Gündüz on 23.10.15.
+//  Copyright © 2015 na. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SVGKitFramework-iOS.
+FOUNDATION_EXPORT double SVGKitFramework_iOSVersionNumber;
+
+//! Project version string for SVGKitFramework-iOS.
+FOUNDATION_EXPORT const unsigned char SVGKitFramework_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SVGKitFramework_iOS/PublicHeader.h>
+
+
---
+++ SVGKitFramework-tvOS/SVGKitFramework-tvOS.h
@@ -0,0 +1,19 @@
+//
+//  SVGKitFramework-tvOS.h
+//  SVGKitFramework-tvOS
+//
+//  Created by Cihat Gündüz on 23.10.15.
+//  Copyright © 2015 na. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SVGKitFramework-tvOS.
+FOUNDATION_EXPORT double SVGKitFramework_tvOSVersionNumber;
+
+//! Project version string for SVGKitFramework-tvOS.
+FOUNDATION_EXPORT const unsigned char SVGKitFramework_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SVGKitFramework_tvOS/PublicHeader.h>
+
+
