6e8dd6aa5e85264e63bfe9d6fcffb1ed5aba9011
Aron Kool <aron.kool@topicus.nl>
2017-03-28 16:25:09 +0200

    Restore if indenting

--- Source/Parsers/SVGKPointsAndPathsParser.m
+++ Source/Parsers/SVGKPointsAndPathsParser.m
@@ -318,13 +318,11 @@ inline BOOL SVGCurveEqualToCurve(SVGCurve curve1, SVGCurve curve2)
 + (void) readCoordinate:(NSScanner*)scanner intoFloat:(CGFloat*) floatPointer
 {
 #if CGFLOAT_IS_DOUBLE
-    
     if( ![scanner scanDouble:floatPointer])
 		NSAssert(FALSE, @"invalid coord");
 #else
-    if( ![scanner scanFloat:floatPointer]){
+    if( ![scanner scanFloat:floatPointer])
 		NSAssert(FALSE, @"invalid coord");
-    }
 #endif
 }
 
