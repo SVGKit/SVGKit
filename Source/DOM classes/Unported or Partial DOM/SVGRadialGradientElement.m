ac43197e39d905508e6d04565a77b44cc0ef01c5
DreamPiggy <lizhuoli1126@126.com>
2018-10-21 15:16:53 +0800

    Remove the `TEMPORARY_WARNING_FOR_APPLES_BROKEN_RENDERINCONTEXT_METHOD`, now most of linear / radial gradients can be rendered on `SVGFastImageView`

--- Source/DOM classes/Unported or Partial DOM/SVGRadialGradientElement.m
+++ Source/DOM classes/Unported or Partial DOM/SVGRadialGradientElement.m
@@ -57,7 +57,7 @@ NSString * const kCAGradientLayerRadial = @"radial";
     SVGLength* svgFR = [SVGLength svgLengthFromNSString:frAttr.length > 0 ? frAttr : @"0%"];
     // This is a tempory workaround. Apple's `CAGradientLayer` does not support focal point for radial gradient. We have to use the low-level API `CGContextDrawRadialGradient` and using custom software-render for focal point. So it does not works for `SVGLayredView` which is hardware-render by CA render server.
     if (fxAttr.length > 0 || fyAttr.length > 0 || frAttr.length > 0) {
-        SVGKitLogVerbose(@"The radialGradient element #%@ contains focal value: (fx:%@, fy: %@, fr:%@). The focul value is only supported on `SVGFastimageView` and it will be ignored when rendering in SVGLayredView.", [self getAttribute:@"id"], fxAttr, fyAttr, frAttr);
+        SVGKitLogWarn(@"The radialGradient element #%@ contains focal value: (fx:%@, fy: %@, fr:%@). The focul value is only supported on `SVGFastimageView` and it will be ignored when rendering in SVGLayredView.", [self getAttribute:@"id"], fxAttr, fyAttr, frAttr);
     }
     self.cx = svgCX;
     self.cy = svgCY;
