a43fd5f0bd2254f41702a8c5d557d416bc7634ec
simon.zeng <simon.zeng@axel.org>
2024-01-04 09:52:44 +0800

    Use DDOSLogger instead of DDASLLogger

--- Source/SVGKit.m
+++ Source/SVGKit.m
@@ -8,12 +8,12 @@
 
 #import "SVGKit.h"
 #import "CocoaLumberjack/DDTTYLogger.h"
-#import "CocoaLumberjack/DDASLLogger.h"
+#import "CocoaLumberjack/DDOSLogger.h"
 
 @implementation SVGKit : NSObject
 
 + (void) enableLogging {
-    [DDLog addLogger:[DDASLLogger sharedInstance]];
+    [DDLog addLogger:[DDOSLogger sharedInstance]];
     [DDLog addLogger:[DDTTYLogger sharedInstance]];
 }
 
