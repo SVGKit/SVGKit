97dfe7826516695d2a5d88c8d170e365903de352
ZeroYoung <135786478+ezeroyoung@users.noreply.github.com>
2024-01-24 18:20:46 +0800

    Fix rendering issues when there are multiple values for text's x or y(#815) (#816)
    Co-authored-by: yangzilong <zlyunduan@bnu.edu.cn>

--- Source/DOM classes/Unported or Partial DOM/SVGTextElement.m
+++ Source/DOM classes/Unported or Partial DOM/SVGTextElement.m
@@ -22,44 +22,120 @@
 	 Because: Apple's classes REQUIRE us to provide a lot of this info up-front. Sigh
 	 And: SVGKit works by pre-baking everything into position (its faster, and avoids Apple's broken CALayer.transform property)
 	 */
-	CGAffineTransform textTransformAbsolute = [SVGHelperUtilities transformAbsoluteIncludingViewportForTransformableOrViewportEstablishingElement:self];
-	/** add on the local x,y that will NOT BE iNCLUDED IN THE TRANSFORM
-	 AUTOMATICALLY BECAUSE THEY ARE NOT TRANSFORM COMMANDS IN SVG SPEC!!
-	 -- but they ARE part of the "implicit transform" of text elements!! (bad SVG Spec design :( )
-	 
-	 NB: the local bits (x/y offset) have to be pre-transformed by
-	 */
+    if (self.x.valueAsString.length != 0 || self.y.valueAsString.length != 0)
+    {
+        // Simplify checks for x and y values being empty or not.
+        NSString *xValue = [self stringValueOrFormatted:self.x.valueAsString withValue:self.x.value];
+        NSString *yValue = [self stringValueOrFormatted:self.y.valueAsString withValue:self.y.value];
+        
+        // Split the text based on the provided X and Y values.
+        NSArray<NSDictionary *> *splitItem = [self splitText:self.textContent basedOnX:xValue andY:yValue];
+        // If no split is needed, create a layer using the entire text content.
+        if (splitItem == nil) {
+            return [self newSubLayerWithX:self.x y:self.y textContent:self.textContent];
+        }
+        // If splitting is needed, create a parent layer and add sublayers for each text segment.
+        CALayer *layer = [CALayer new];
+        for (NSDictionary *item in splitItem)
+        {
+            CALayer *subLayer = [self newSubLayerWithX:[SVGLength svgLengthFromNSString:item[@"x"]]
+                                                    y:[SVGLength svgLengthFromNSString:item[@"y"]]
+                                          textContent:item[@"text"]];
+            [layer addSublayer:subLayer];
+        }
+        return layer;
+    }
+    
+    // If no position is specified, create a layer using the current X and Y values and the entire text content.
+    return [self newSubLayerWithX:self.x y:self.y textContent:self.textContent];
+}
+
+/// Helper method to return the string value or its formatted version if empty.
+- (NSString *)stringValueOrFormatted:(NSString *)valueString withValue:(CGFloat)value
+{
+    return valueString.length > 0 ? valueString : [NSString stringWithFormat:@"%f", value];
+}
+
+/// Method to split the text into segments based on X and Y position strings.
+/// This allows for text to be distributed across different positions if specified.
+- (NSArray<NSDictionary *> *)splitText:(NSString *)text basedOnX:(NSString *)xString andY:(NSString *)yString
+{
+    // Separate the X and Y position strings into arrays of individual positions.
+    NSArray *xValues = [xString componentsSeparatedByString:@" "];
+    NSArray *yValues = [yString componentsSeparatedByString:@" "];
+    // Determine the number of splits based on the greater count of X or Y positions.
+    NSInteger splitCount = MAX(xValues.count, yValues.count);
+    if (splitCount == 0) {
+        return nil; // If no splitting is needed, return nil.
+    }
+
+    // Prepare an array to hold the results of the split.
+    NSMutableArray<NSDictionary *> *result = [NSMutableArray array];
+
+    NSString *previousNonNullableX = @"";
+    NSString *previousNonNullableY = @"";
+    for (NSInteger i = 0; i < splitCount; i++) {
+        // Use the current or previous non-empty X and Y positions.
+        NSString *xPart = i < xValues.count ? xValues[i] : previousNonNullableX;
+        previousNonNullableX = xPart;
+        NSString *yPart = i < yValues.count ? yValues[i] : previousNonNullableY;
+        previousNonNullableY = yPart;
+        
+        // Split the text into parts according to the current index and remaining length.
+        NSString *textPart = @"";
+        if (i < text.length) {
+            NSRange range = NSMakeRange(i, i == (splitCount - 1) ? (text.length - i) : 1);
+            textPart = [text substringWithRange:range];
+        }
+
+        // Create a dictionary for each text segment with its corresponding X and Y positions.
+        NSDictionary *dict = @{@"x": xPart, @"y": yPart, @"text": textPart};
+        [result addObject:dict];
+    }
+
+    return result; // Return the array of dictionaries containing split text and positions.
+}
+
+- (CALayer *) newSubLayerWithX:(SVGLength *)x y:(SVGLength *)y textContent:(NSString *)textContent
+{
+    CGAffineTransform textTransformAbsolute = [SVGHelperUtilities transformAbsoluteIncludingViewportForTransformableOrViewportEstablishingElement:self];
+    /** add on the local x,y that will NOT BE iNCLUDED IN THE TRANSFORM
+     AUTOMATICALLY BECAUSE THEY ARE NOT TRANSFORM COMMANDS IN SVG SPEC!!
+     -- but they ARE part of the "implicit transform" of text elements!! (bad SVG Spec design :( )
+     
+     NB: the local bits (x/y offset) have to be pre-transformed by
+     */
     CGRect viewport = CGRectFromSVGRect(self.rootOfCurrentDocumentFragment.viewBox);
-	CGAffineTransform textTransformAbsoluteWithLocalPositionOffset = CGAffineTransformConcat( CGAffineTransformMakeTranslation( [self.x pixelsValueWithDimension:viewport.size.width], [self.y pixelsValueWithDimension:viewport.size.height]), textTransformAbsolute);
-	
-	/**
-	 Apple's CATextLayer is poor - one of those classes Apple hasn't finished writing?
-	 
-	 It's incompatible with UIFont (Apple states it is so), and it DOES NOT WORK by default:
-	 
-	 If you assign a font, and a font size, and text ... you get a blank empty layer of
-	 size 0,0
-	 
-	 Because Apple requires you to ALSO do all the work of calculating the font size, shape,
-	 position etc.
-	 
-	 But its the easiest way to get FULL control over size/position/rotation/etc in a CALayer
-	 */
+    CGAffineTransform textTransformAbsoluteWithLocalPositionOffset = CGAffineTransformConcat( CGAffineTransformMakeTranslation( [x pixelsValueWithDimension:viewport.size.width], [y pixelsValueWithDimension:viewport.size.height]), textTransformAbsolute);
+    
+    /**
+     Apple's CATextLayer is poor - one of those classes Apple hasn't finished writing?
+     
+     It's incompatible with UIFont (Apple states it is so), and it DOES NOT WORK by default:
+     
+     If you assign a font, and a font size, and text ... you get a blank empty layer of
+     size 0,0
+     
+     Because Apple requires you to ALSO do all the work of calculating the font size, shape,
+     position etc.
+     
+     But its the easiest way to get FULL control over size/position/rotation/etc in a CALayer
+     */
     
     /**
      Create font based on many information (font-family, font-weight, etc), fallback to system font when there are no available font matching the information.
      */
     UIFont *font = [SVGTextElement matchedFontWithElement:self];
-	
-	/** Convert the size down using the SVG transform at this point, before we calc the frame size etc */
-//	effectiveFontSize = CGSizeApplyAffineTransform( CGSizeMake(0,effectiveFontSize), textTransformAbsolute ).height; // NB important that we apply a transform to a "CGSize" here, so that Apple's library handles worrying about whether to ignore skew transforms etc
+    
+    /** Convert the size down using the SVG transform at this point, before we calc the frame size etc */
+//    effectiveFontSize = CGSizeApplyAffineTransform( CGSizeMake(0,effectiveFontSize), textTransformAbsolute ).height; // NB important that we apply a transform to a "CGSize" here, so that Apple's library handles worrying about whether to ignore skew transforms etc
 
-	/** Convert all whitespace to spaces, and trim leading/trailing (SVG doesn't support leading/trailing whitespace, and doesnt support CR LF etc) */
-	
-	NSString* effectiveText = self.textContent; // FIXME: this is a TEMPORARY HACK, UNTIL PROPER PARSING OF <TSPAN> ELEMENTS IS ADDED
-	
-	effectiveText = [effectiveText stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
-	effectiveText = [effectiveText stringByReplacingOccurrencesOfString:@"\n" withString:@" "];
+    /** Convert all whitespace to spaces, and trim leading/trailing (SVG doesn't support leading/trailing whitespace, and doesnt support CR LF etc) */
+    
+    NSString* effectiveText = textContent;
+    
+    effectiveText = [effectiveText stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    effectiveText = [effectiveText stringByReplacingOccurrencesOfString:@"\n" withString:@" "];
     
     /**
      Stroke color && stroke width
@@ -80,17 +156,17 @@
      Apple's `CATextLayer` can be filled using mask.
      */
     CGColorRef fillColor = [SVGHelperUtilities parseFillForElement:self];
-	
-	/** Calculate 
-	 
-	 1. Create an attributed string (Apple's APIs are hard-coded to require this)
-	 2. Set the font to be the correct one + correct size for whole string, inside the string
-	 3. Ask apple how big the final thing should be
-	 4. Use that to provide a layer.frame
-	 */
-	NSMutableAttributedString* attributedString = [[NSMutableAttributedString alloc] initWithString:effectiveText];
+    
+    /** Calculate
+     
+     1. Create an attributed string (Apple's APIs are hard-coded to require this)
+     2. Set the font to be the correct one + correct size for whole string, inside the string
+     3. Ask apple how big the final thing should be
+     4. Use that to provide a layer.frame
+     */
+    NSMutableAttributedString* attributedString = [[NSMutableAttributedString alloc] initWithString:effectiveText];
     NSRange stringRange = NSMakeRange(0, attributedString.string.length);
-	[attributedString addAttribute:NSFontAttributeName
+    [attributedString addAttribute:NSFontAttributeName
                              value:font
                              range:stringRange];
     if (fillColor) {
@@ -112,55 +188,55 @@
                                  value:@(strokeValue)
                                  range:stringRange];
     }
-	CTFramesetterRef framesetter = CTFramesetterCreateWithAttributedString( (CFMutableAttributedStringRef) attributedString );
+    CTFramesetterRef framesetter = CTFramesetterCreateWithAttributedString( (CFMutableAttributedStringRef) attributedString );
     CGSize suggestedUntransformedSize = CTFramesetterSuggestFrameSizeWithConstraints(framesetter, CFRangeMake(0, 0), NULL, CGSizeMake(CGFLOAT_MAX, CGFLOAT_MAX), NULL);
     CFRelease(framesetter);
-	
-	CGRect unTransformedFinalBounds = CGRectMake( 0,
-											  0,
-											  suggestedUntransformedSize.width,
-											  suggestedUntransformedSize.height); // everything's been pre-scaled by [self transformAbsolute]
-	
+    
+    CGRect unTransformedFinalBounds = CGRectMake( 0,
+                                              0,
+                                              suggestedUntransformedSize.width,
+                                              suggestedUntransformedSize.height); // everything's been pre-scaled by [self transformAbsolute]
+    
     CATextLayer *label = [SVGTextLayer layer];
     [SVGHelperUtilities configureCALayer:label usingElement:self];
-	
-	/** This is complicated for three reasons.
-	 Partly: Apple and SVG use different defitions for the "origin" of a piece of text
-	 Partly: Bugs in Apple's CoreText
-	 Partly: flaws in Apple's CALayer's handling of frame,bounds,position,anchorPoint,affineTransform
-	 
-	 1. CALayer.frame DOES NOT EXIST AS A REAL PROPERTY - if you read Apple's docs you eventually realise it is fake. Apple explicitly says it is "not defined". They should DELETE IT from their API!
-	 2. CALayer.bounds and .position ARE NOT AFFECTED BY .affineTransform - only the contents of the layer is affected
-	 3. SVG defines two SEMI-INCOMPATIBLE ways of positioning TEXT objects, that we have to correctly combine here.
-	 4. So ... to apply a transform to the layer text:
-	     i. find the TRANSFORM
-	     ii. merge it with the local offset (.x and .y from SVG) - which defaults to (0,0)
-	     iii. apply that to the layer
-	     iv. set the position to 0
-	     v. BECAUSE SVG AND APPLE DEFINE ORIGIN DIFFERENTLY: subtract the "untransformed" height of the font ... BUT: pre-transformed ONLY BY the 'multiplying (non-translating)' part of the TRANSFORM.
-	     vi. set the bounds to be (whatever Apple's CoreText says is necessary to render TEXT at FONT SIZE, with NO TRANSFORMS)
-	 */
+    
+    /** This is complicated for three reasons.
+     Partly: Apple and SVG use different defitions for the "origin" of a piece of text
+     Partly: Bugs in Apple's CoreText
+     Partly: flaws in Apple's CALayer's handling of frame,bounds,position,anchorPoint,affineTransform
+     
+     1. CALayer.frame DOES NOT EXIST AS A REAL PROPERTY - if you read Apple's docs you eventually realise it is fake. Apple explicitly says it is "not defined". They should DELETE IT from their API!
+     2. CALayer.bounds and .position ARE NOT AFFECTED BY .affineTransform - only the contents of the layer is affected
+     3. SVG defines two SEMI-INCOMPATIBLE ways of positioning TEXT objects, that we have to correctly combine here.
+     4. So ... to apply a transform to the layer text:
+         i. find the TRANSFORM
+         ii. merge it with the local offset (.x and .y from SVG) - which defaults to (0,0)
+         iii. apply that to the layer
+         iv. set the position to 0
+         v. BECAUSE SVG AND APPLE DEFINE ORIGIN DIFFERENTLY: subtract the "untransformed" height of the font ... BUT: pre-transformed ONLY BY the 'multiplying (non-translating)' part of the TRANSFORM.
+         vi. set the bounds to be (whatever Apple's CoreText says is necessary to render TEXT at FONT SIZE, with NO TRANSFORMS)
+     */
     label.bounds = unTransformedFinalBounds;
-	
-	/** NB: specific to Apple: the "origin" is the TOP LEFT corner of first line of text, whereas SVG uses the font's internal origin
-	 (which is BOTTOM LEFT CORNER OF A LETTER SUCH AS 'a' OR 'x' THAT SITS ON THE BASELINE ... so we have to make the FRAME start "font leading" higher up
-	 
-	 WARNING: Apple's font-rendering system has some nasty bugs (c.f. StackOverflow)
-	 
-	 We TRIED to use the font's built-in numbers to correct the position, but Apple's own methods often report incorrect values,
-	 and/or Apple has deprecated REQUIRED methods in their API (with no explanation - e.g. "font leading")
-	 
-	 If/when Apple fixes their bugs - or if you know enough about their API's to workaround the bugs, feel free to fix this code.
-	 */
+    
+    /** NB: specific to Apple: the "origin" is the TOP LEFT corner of first line of text, whereas SVG uses the font's internal origin
+     (which is BOTTOM LEFT CORNER OF A LETTER SUCH AS 'a' OR 'x' THAT SITS ON THE BASELINE ... so we have to make the FRAME start "font leading" higher up
+     
+     WARNING: Apple's font-rendering system has some nasty bugs (c.f. StackOverflow)
+     
+     We TRIED to use the font's built-in numbers to correct the position, but Apple's own methods often report incorrect values,
+     and/or Apple has deprecated REQUIRED methods in their API (with no explanation - e.g. "font leading")
+     
+     If/when Apple fixes their bugs - or if you know enough about their API's to workaround the bugs, feel free to fix this code.
+     */
     CTLineRef line = CTLineCreateWithAttributedString( (CFMutableAttributedStringRef) attributedString );
     CGFloat ascent = 0;
     CTLineGetTypographicBounds(line, &ascent, NULL, NULL);
     CFRelease(line);
-	CGFloat offsetToConvertSVGOriginToAppleOrigin = -ascent;
-	CGSize fakeSizeToApplyNonTranslatingPartsOfTransform = CGSizeMake( 0, offsetToConvertSVGOriginToAppleOrigin);
-	
-	label.position = CGPointMake( 0,
-								 0 + CGSizeApplyAffineTransform( fakeSizeToApplyNonTranslatingPartsOfTransform, textTransformAbsoluteWithLocalPositionOffset).height);
+    CGFloat offsetToConvertSVGOriginToAppleOrigin = -ascent;
+    CGSize fakeSizeToApplyNonTranslatingPartsOfTransform = CGSizeMake( 0, offsetToConvertSVGOriginToAppleOrigin);
+    
+    label.position = CGPointMake( 0,
+                                 0 + CGSizeApplyAffineTransform( fakeSizeToApplyNonTranslatingPartsOfTransform, textTransformAbsoluteWithLocalPositionOffset).height);
     
     NSString *textAnchor = [self cascadedValueForStylableProperty:@"text-anchor"];
     if( [@"middle" isEqualToString:textAnchor] )
@@ -170,7 +246,7 @@
     else
         label.anchorPoint = CGPointZero; // WARNING: SVG applies transforms around the top-left as origin, whereas Apple defaults to center as origin, so we tell Apple to work "like SVG" here.
     
-	label.affineTransform = textTransformAbsoluteWithLocalPositionOffset;
+    label.affineTransform = textTransformAbsoluteWithLocalPositionOffset;
     label.string = [attributedString copy];
     label.alignmentMode = kCAAlignmentLeft;
     
@@ -181,12 +257,11 @@
 #endif
     
     return [self newCALayerForTextLayer:label transformAbsolute:textTransformAbsolute];
-
-	/** VERY USEFUL when trying to debug text issues:
-	label.backgroundColor = [UIColor colorWithRed:0.5 green:0 blue:0 alpha:0.5].CGColor;
-	label.borderColor = [UIColor redColor].CGColor;
-	//DEBUG: SVGKitLogVerbose(@"font size %2.1f at %@ ... final frame of layer = %@", effectiveFontSize, NSStringFromCGPoint(transformedOrigin), NSStringFromCGRect(label.frame));
-	*/
+    /** VERY USEFUL when trying to debug text issues:
+    label.backgroundColor = [UIColor colorWithRed:0.5 green:0 blue:0 alpha:0.5].CGColor;
+    label.borderColor = [UIColor redColor].CGColor;
+    //DEBUG: SVGKitLogVerbose(@"font size %2.1f at %@ ... final frame of layer = %@", effectiveFontSize, NSStringFromCGPoint(transformedOrigin), NSStringFromCGRect(label.frame));
+    */
 }
 
 -(CALayer *) newCALayerForTextLayer:(CATextLayer *)label transformAbsolute:(CGAffineTransform)transformAbsolute
