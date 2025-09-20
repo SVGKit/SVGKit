SVGKit/SVGKit

commit a26fb848b01bd9580f2980d191923b7ca02fd8be

FIXED: SVGTextElement now works correctly when the SVG specifies the font name instead of the family name. (#859)
Co-authored-by: Moisés Moreno <mmoreno@iteration-mobile.com>

Source/DOM classes/Unported or Partial DOM/SVGTextElement.m
@@ -326,6 +326,19 @@
                 break;
             }
         }
+
+        // some SVGs specify font name instead family name
+        if (!matchedFontFamily) {
+            // we didn't found font family, let's search the family based on the font name
+            for (NSString *familyName in availableFontFamilies) {
+                for (NSString *fontName in [UIFont fontNamesForFamilyName:familyName]) {
+                    if ([actualFontFamilies containsObject:fontName]) {
+                        matchedFontFamily = familyName;
+                        break;
+                    }
+                }
+            }
+        }
     }
     
     // we provide enough hint information, let Core Text using their algorithm to detect which fontName should be used

commit b503244b506bbf44e6c3d45fe01e7063abc2ec98

Add missing macOS conditional to fix compilation (broken in #859 / a26fb84)

Source/DOM classes/Unported or Partial DOM/SVGTextElement.m
@@ -327,16 +327,30 @@
             }
         }
 
-        // some SVGs specify font name instead family name
+        // Some SVGs specify font name instead of family name
         if (!matchedFontFamily) {
-            // we didn't found font family, let's search the family based on the font name
+            // If we didn't find the font family, we'll search the family based on the font name
             for (NSString *familyName in availableFontFamilies) {
+                #if SVGKIT_MAC
+                NSArray<NSArray *> *members = [NSFontManager.sharedFontManager availableMembersOfFontFamily:familyName];
+                for (NSArray *member in members) {
+                    // Each member has the format: [fontName, weight, traits, size]
+                    if (member.count > 0) {
+                        NSString *fontName = member[0];
+                        if ([actualFontFamilies containsObject:fontName]) {
+                            matchedFontFamily = familyName;
+                            break;
+                        }
+                    }
+                }
+#else
                 for (NSString *fontName in [UIFont fontNamesForFamilyName:familyName]) {
                     if ([actualFontFamilies containsObject:fontName]) {
                         matchedFontFamily = familyName;
                         break;
                     }
                 }
+#endif
             }
         }
     }